abd157b883c9a933085cb4fc29335e6908d7f671
Jon Huh <oneandwholly@gmail.com>
2017-05-16 20:26:11 +0900

    expect CommentBox component to have className comment-box

--- src/components/comment_box.js
+++ src/components/comment_box.js
@@ -3,9 +3,9 @@ import React, { Component } from 'react';
 export default class CommentBox extends Component {
   render() {
     return (
-      <div>
-      <textarea />
-      <button>Submit Commnet</button>
+      <div className='comment-box'>
+        <textarea />
+        <button>Submit Commnet</button>
       </div>
     );
   }
